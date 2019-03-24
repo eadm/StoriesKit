4a231ca02d129f0b8b53711fa96032da260779e0
Ruslan Davletshin <smartmorefeed@gmail.com>
2019-03-24 22:32:45 +0300

    add stories fragment delegate base

---
+++ storieskit/src/main/java/ru/nobird/android/stories/ui/delegate/StoriesFragmentDelegateBase.kt
@@ -0,0 +1,137 @@
+package ru.nobird.android.stories.ui.delegate
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.doOnPreDraw
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
+import ru.nobird.android.stories.model.Story
+import ru.nobird.android.stories.transition.SharedTransitionIntentBuilder
+import ru.nobird.android.stories.ui.adapter.StoriesPagerAdapter
+import ru.nobird.android.stories.ui.custom.DismissableLayout
+import ru.nobird.android.stories.ui.custom.StoryView
+
+abstract class StoriesFragmentDelegateBase(
+    private val fragment: Fragment
+) {
+    protected abstract val dismissableLayout: DismissableLayout
+    protected abstract val storiesViewPager: ViewPager
+
+    protected abstract val arguments: Bundle
+
+    protected abstract val storyPartDelegates: List<StoryPartViewDelegate>
+
+    private lateinit var stories: List<Story>
+
+    fun onCreate(savedInstanceState: Bundle?) {
+        stories = arguments.getParcelableArrayList(SharedTransitionIntentBuilder.EXTRA_STORIES)!!
+
+        initStoriesPager()
+
+        dismissableLayout.content = storiesViewPager
+        dismissableLayout.addDismissListener(object : DismissableLayout.DismissListener {
+            override fun onDragCancelled() {
+                storiesViewPager.findViewWithTag<StoryView>(storiesViewPager.currentItem)?.resume()
+            }
+
+            override fun onDismiss() {
+                finish()
+            }
+        })
+
+        val sharedTransitionDelegate = fragment.targetFragment as? SharedTransitionContainerDelegate
+
+        if (savedInstanceState == null) {
+            val position = arguments.getInt(SharedTransitionIntentBuilder.EXTRA_POSITION)
+            storiesViewPager.setCurrentItem(position, false)
+
+            dismissableLayout.visibility = View.INVISIBLE
+            dismissableLayout.doOnPreDraw {
+                val view = sharedTransitionDelegate?.getSharedView(storiesViewPager.currentItem)
+                if (view != null) {
+                    dismissableLayout.playEnterAnimation(view) {
+                        sharedTransitionDelegate.onPositionChanged(position)
+                        storiesViewPager.findViewWithTag<StoryView>(position)?.resume()
+                    }
+                } else {
+                    sharedTransitionDelegate?.onPositionChanged(position)
+                    storiesViewPager.findViewWithTag<StoryView>(position)?.resume()
+                }
+            }
+        } else {
+            sharedTransitionDelegate?.onPositionChanged(storiesViewPager.currentItem)
+        }
+    }
+
+    private fun initStoriesPager() {
+        storiesViewPager.adapter = StoriesPagerAdapter(stories, storyPartDelegates, object : StoryView.StoryProgressListener {
+            override fun onNext() {
+                if (storiesViewPager.currentItem == (storiesViewPager.adapter?.count ?: 0) - 1) {
+                    onComplete()
+                }
+                storiesViewPager.currentItem++
+            }
+
+            override fun onPrev() {
+                storiesViewPager.currentItem--
+            }
+        })
+
+        storiesViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    storiesViewPager
+                        .findViewWithTag<StoryView>(storiesViewPager.currentItem)
+                        ?.resume()
+                }
+            }
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                (fragment.targetFragment as? SharedTransitionContainerDelegate)
+                    ?.onPositionChanged(position)
+
+                storiesViewPager.findViewWithTag<StoryView>(position)?.restartCurrentPart()
+            }
+        })
+    }
+
+    fun onResume() {
+        storiesViewPager
+            .findViewWithTag<StoryView>(storiesViewPager.currentItem)
+            ?.resume()
+    }
+
+    fun onPause() {
+        storiesViewPager
+            .findViewWithTag<StoryView>(storiesViewPager.currentItem)
+            ?.pause()
+    }
+
+    /**
+     * Calls when stories completed
+     */
+    protected open fun onComplete() {
+        finish()
+    }
+
+    open fun finish() {
+        val sharedTransitionDelegate = fragment.targetFragment as? SharedTransitionContainerDelegate
+
+        val view = sharedTransitionDelegate?.getSharedView(storiesViewPager.currentItem)
+        if (view == null) {
+            sharedTransitionDelegate?.onPositionChanged(-1)
+            onClose()
+        } else {
+            dismissableLayout.playExitAnimation(view) {
+                sharedTransitionDelegate?.onPositionChanged(-1)
+                onClose()
+            }
+        }
+    }
+
+    /**
+     * Close current screen. Due to different possible navigation implementations this should be implemented by user.
+     */
+    protected abstract fun onClose()
+}
