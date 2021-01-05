bbc9c4e900c54bb82b70b57958e77d83a5abc029
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-01-05 12:17:01 +0300

    remove support lib

--- storieskitsupport/src/main/java/ru/nobird/android/stories/model/StoryPart.kt
+++
@@ -1,8 +0,0 @@
-package ru.nobird.android.stories.model
-
-import android.os.Parcelable
-
-abstract class StoryPart(
-        val duration: Long,
-        val cover: String
-) : Parcelable
--- storieskitsupport/src/main/java/ru/nobird/android/stories/ui/delegate/SharedTransitionContainerDelegate.kt
+++
@@ -1,8 +0,0 @@
-package ru.nobird.android.stories.ui.delegate
-
-import android.view.View
-
-interface SharedTransitionContainerDelegate {
-    fun getSharedView(position: Int): View?
-    fun onPositionChanged(position: Int)
-}
